1d50c6c762dff10c19aa478fb717b57d106801e3
rylopez <yohanny_116@hotmail.com>
2017-03-23 17:36:44 -0500

    algo

--- view/inicio.php
+++ view/inicio.php
@@ -33,6 +33,114 @@
         </div>
     </div>
 </div>
+<!-- Portfolio Section -->
+<div id="works">
+  <div class="container"> <!-- Container -->
+    <div class="section-title text-center center">
+      <h2>Our Portfolio</h2>
+      <hr>
+      <div class="clearfix"></div>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis sed dapibus leo nec ornare diamcommodo nibh ante facilisis.</p>
+    </div>
+    <div class="categories">
+      <ul class="cat">
+        <li>
+          <ol class="type">
+            <li><a href="#" data-filter="*" class="active">All</a></li>
+            <li><a href="#" data-filter=".lorem">Web Design</a></li>
+            <li><a href="#" data-filter=".consectetur">Web Development</a></li>
+            <li><a href="#" data-filter=".dapibus">Branding</a></li>
+          </ol>
+        </li>
+      </ul>
+      <div class="clearfix"></div>
+    </div>
+    <div class="row">
+      <div class="portfolio-items">
+        <div class="col-xs-6 col-sm-6 col-md-3 col-lg-3 lorem">
+          <div class="portfolio-item">
+            <div class="hover-bg"> <a href="../Nuevacarpeta/img/portfolio/01.jpg" title="Project description" rel="prettyPhoto">
+              <div class="hover-text">
+                <h4>Project Title</h4>
+                <p>Web Design</p>
+              </div>
+              <img src="../Nuevacarpeta/img/portfolio/01.jpg" class="img-responsive" alt="Project Title"> </a> </div>
+          </div>
+        </div>
+        <div class="col-xs-6 col-sm-6 col-md-3 col-lg-3 consectetur">
+          <div class="portfolio-item">
+            <div class="hover-bg"> <a href="../Nuevacarpeta/img/portfolio/02.jpg" title="Project description" rel="prettyPhoto">
+              <div class="hover-text">
+                <h4>Project Title</h4>
+                <p>Web Development</p>
+              </div>
+              <img src="../Nuevacarpeta/img/portfolio/02.jpg" class="img-responsive" alt="Project Title"> </a> </div>
+          </div>
+        </div>
+        <div class="col-xs-6 col-sm-6 col-md-3 col-lg-3 lorem">
+          <div class="portfolio-item">
+            <div class="hover-bg"> <a href="../Nuevacarpeta/img/portfolio/03.jpg" title="Project description" rel="prettyPhoto">
+              <div class="hover-text">
+                <h4>Project Title</h4>
+                <p>Web Design</p>
+              </div>
+              <img src="../Nuevacarpeta/img/portfolio/03.jpg" class="img-responsive" alt="Project Title"> </a> </div>
+          </div>
+        </div>
+        <div class="col-xs-6 col-sm-6 col-md-3 col-lg-3 lorem">
+          <div class="portfolio-item">
+            <div class="hover-bg"> <a href="../Nuevacarpeta/img/portfolio/04.jpg" title="Project description" rel="prettyPhoto">
+              <div class="hover-text">
+                <h4>Project Title</h4>
+                <p>Web Design</p>
+              </div>
+              <img src="../Nuevacarpeta/img/portfolio/04.jpg" class="img-responsive" alt="Project Title"> </a> </div>
+          </div>
+        </div>
+        <div class="col-xs-6 col-sm-6 col-md-3 col-lg-3 consectetur">
+          <div class="portfolio-item">
+            <div class="hover-bg"> <a href="../Nuevacarpeta/img/portfolio/05.jpg" title="Project description" rel="prettyPhoto">
+              <div class="hover-text">
+                <h4>Project Title</h4>
+                <p>Web Development</p>
+              </div>
+              <img src="../Nuevacarpeta/img/portfolio/05.jpg" class="img-responsive" alt="Project Title"> </a> </div>
+          </div>
+        </div>
+        <div class="col-xs-6 col-sm-6 col-md-3 col-lg-3 dapibus">
+          <div class="portfolio-item">
+            <div class="hover-bg"> <a href="../Nuevacarpeta/img/portfolio/06.jpg" title="Project description" rel="prettyPhoto">
+              <div class="hover-text">
+                <h4>Project Title</h4>
+                <p>Branding</p>
+              </div>
+              <img src="../Nuevacarpeta/img/portfolio/06.jpg" class="img-responsive" alt="Project Title"> </a> </div>
+          </div>
+        </div>
+        <div class="col-xs-6 col-sm-6 col-md-3 col-lg-3 dapibus consectetur">
+          <div class="portfolio-item">
+            <div class="hover-bg"> <a href="../Nuevacarpeta/img/portfolio/07.jpg" title="Project description" rel="prettyPhoto">
+              <div class="hover-text">
+                <h4>Project Title</h4>
+                <p>Web Development, Branding</p>
+              </div>
+              <img src="../Nuevacarpeta/img/portfolio/07.jpg" class="img-responsive" alt="Project Title"> </a> </div>
+          </div>
+        </div>
+        <div class="col-xs-6 col-sm-6 col-md-3 col-lg-3 lorem">
+          <div class="portfolio-item">
+            <div class="hover-bg"> <a href="../Nuevacarpeta/img/portfolio/08.jpg" title="Project description" rel="prettyPhoto">
+              <div class="hover-text">
+                <h4>Project Title</h4>
+                <p>Web Design</p>
+              </div>
+              <img src="../Nuevacarpeta/img/portfolio/08.jpg" class="img-responsive" alt="Project Title"> </a> </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
 <div class="container" id="content">
     <!-- Example row of columns -->
     <div class="row">
